63e47263e55fb4677ba344f6ef0ce0ef42eb4741
Helge  Johnsen <32477331+helgovic@users.noreply.github.com>
2022-03-14 09:54:17 +0100

    Add files via upload

---
+++ Source/WinBroadlink.dpr
@@ -0,0 +1,51 @@
+program WinBroadlink;
+
+uses
+  Vcl.Forms,
+  UFBroadlink in 'UFBroadlink.pas' {FBroadlink},
+  Vcl.Themes,
+  Vcl.Styles,
+  UFAddDevice in 'UFAddDevice.pas' {FAddDevice},
+  UFAddButton in 'UFAddButton.pas' {FAddButton},
+  UFBLDev in 'UFBLDev.pas' {FBLDevName},
+  UFMacros in 'UFMacros.pas' {FMacros},
+  UFNewMacro in 'UFNewMacro.pas' {FNewMacro},
+  UFAddMacroButton in 'UFAddMacroButton.pas' {FAddMacroButton},
+  UFSetUpBl in 'UFSetUpBl.pas' {FSetUpBL},
+  UFInstructions in 'UFInstructions.pas' {FInstructions},
+  UDMBroadlink in 'UDMBroadlink.pas' {DMBroadlink: TDataModule},
+  UFShareInfo in 'UFShareInfo.pas' {FShareInfo},
+  UFAddLightButton in 'UFAddLightButton.pas' {FAddLightButton},
+  ColorConv in '..\Utils\ColorConv.pas',
+  RGBHSV in '..\Utils\RGBHSV.pas',
+  UFTimers in 'UFTimers.pas' {FTimers},
+  UFrTimer in 'UFrTimer.pas' {FrTimer: TFrame},
+  UFAddTimer in 'UFAddTimer.pas' {FAddTimer},
+  UFLocations in 'UFLocations.pas' {FLocations},
+  UFNewLocation in 'UFNewLocation.pas' {FNewLocation},
+  GpTimezone in '..\Utils\GpTimezone.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Aqua Graphite');
+  Application.CreateForm(TDMBroadlink, DMBroadlink);
+  Application.CreateForm(TFBroadlink, FBroadlink);
+  Application.CreateForm(TFAddDevice, FAddDevice);
+  Application.CreateForm(TFAddButton, FAddButton);
+  Application.CreateForm(TFBLDevName, FBLDevName);
+  Application.CreateForm(TFMacros, FMacros);
+  Application.CreateForm(TFNewMacro, FNewMacro);
+  Application.CreateForm(TFAddMacroButton, FAddMacroButton);
+  Application.CreateForm(TFSetUpBL, FSetUpBL);
+  Application.CreateForm(TFInstructions, FInstructions);
+  Application.CreateForm(TFShareInfo, FShareInfo);
+  Application.CreateForm(TFAddLightButton, FAddLightButton);
+  Application.CreateForm(TFTimers, FTimers);
+  Application.CreateForm(TFAddTimer, FAddTimer);
+  Application.CreateForm(TFLocations, FLocations);
+  Application.CreateForm(TFNewLocation, FNewLocation);
+  Application.Run;
+end.
